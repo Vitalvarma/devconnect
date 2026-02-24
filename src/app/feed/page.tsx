ce5f60ffd8d96b7f2c704a8bd6fdfd5290e9744d
Vitalvarma <vitalvarma13@gmail.com>
2026-02-24 12:07:21 +0530

    fixed feed page user.email to user.id for dynamic routing using user id

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,8 +1,21 @@
 import { prisma } from "@/lib/prisma";
 import CreatePost from "@/components/CreatePost";
 import LikeButton from "@/components/LIkeButton";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function FeedPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user?.id) {
+    return (
+      <div style={{ padding: 20 }}>
+        <h1>Please login to view the feed</h1>
+      </div>
+    );
+  }
+
   const posts = await prisma.post.findMany({
     include: {
       author: true,
@@ -17,18 +30,26 @@ export default async function FeedPage() {
     <div style={{ padding: 20 }}>
       <h1>Feed 🚀</h1>
 
+      <Link href={`/profile/${session.user.id}`}>
+        View My Profile
+      </Link>
+
       {posts.map((post) => (
-        <div key={post.id} style={{ border: "1px solid #ccc", margin: 10, padding: 10 }}>
+        <div
+          key={post.id}
+          style={{ border: "1px solid #ccc", margin: 10, padding: 10 }}
+        >
           <p>{post.content}</p>
           <small>By: {post.author.email}</small>
+
           <LikeButton
-    postId={post.id}
-    initialCount={post.likes.length}/>
+            postId={post.id}
+            initialCount={post.likes.length}
+          />
         </div>
       ))}
 
       <CreatePost />
-
     </div>
   );
 }
